07e9d24697c56aaaa722c6e968401c76c3381f08
fmatovelle <167099575+fmatovelle@users.noreply.github.com>
2024-08-07 21:59:46 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  // Función para generar un número aleatorio
+  function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  // Generar excusa
+  function generateExcuse() {
+    let whoIndex = getRandomIndex(who);
+    let actionIndex = getRandomIndex(action);
+    let whatIndex = getRandomIndex(what);
+    let whenIndex = getRandomIndex(when);
+
+    return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+  }
+
+  // Asignar excusa generada al elemento HTML
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
